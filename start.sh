bbfad0aca3c07c05954e704c7b02f5359b670247
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-18 04:03:38 +0200

    Colours are always good. ;P

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
+############ COLOURED BASH TEXT
+
+# ANSI color codes
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+PURPLE='\033[0;35m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+###############################
+
 ###
 ### This just starts the other scripts.
 ###
@@ -12,4 +25,4 @@ sh $LOC/files/configure.network.shh
 
 sh $LOC/files/configure.network-shares.shh
 
-echo "DONE!"
+echo -e "${PURPLE} DONE! ${NC}"
